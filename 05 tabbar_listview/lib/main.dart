31ab0f3b1ff8b29abde6e85b6f87895942686df0
jisora <srji@genuinecnp.com>
2021-09-13 18:03:50 +0900

    210913 탭바와 리스트 만들기

---
+++ 05 tabbar_listview/lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:tabbar_listview/animalItem.dart';
+import 'package:tabbar_listview/sub/firstPage.dart';
+import 'package:tabbar_listview/sub/secondPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  List<Animal> animalList = new List.empty(growable: true);
+  TabController? controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+    controller!.addListener(() {
+      if (controller!.indexIsChanging)
+        print("이전 index: ${controller!.previousIndex}");
+      print("현재 index: ${controller!.index}");
+    });
+
+    animalList.add(
+        Animal(animalName: "벌", kind: "곤충", imagePath: "repo/images/bee.png"));
+    animalList.add(Animal(
+        animalName: "고양이", kind: "포유류", imagePath: "repo/images/cat.png"));
+    animalList.add(Animal(
+        animalName: "젖소", kind: "포유류", imagePath: "repo/images/cow.png"));
+    animalList.add(Animal(
+        animalName: "강아지", kind: "포유류", imagePath: "repo/images/dog.png"));
+    animalList.add(Animal(
+        animalName: "여우", kind: "포유류", imagePath: "repo/images/fox.png"));
+    animalList.add(Animal(
+        animalName: "원숭이", kind: "영장류", imagePath: "repo/images/monkey.png"));
+    animalList.add(Animal(
+        animalName: "돼지", kind: "포유류", imagePath: "repo/images/pig.png"));
+    animalList.add(Animal(
+        animalName: "늑대", kind: "포유류", imagePath: "repo/images/wolf.png"));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TabBar Example'),
+      ),
+      body: TabBarView(
+        children: <Widget>[
+          FirstApp(
+            list: animalList,
+          ),
+          SecondApp()
+        ],
+        controller: controller,
+      ),
+      bottomNavigationBar: TabBar(
+        tabs: <Tab>[
+          Tab(
+            icon: Icon(
+              Icons.looks_one,
+              color: Colors.blue,
+            ),
+          ),
+          Tab(
+            icon: Icon(
+              Icons.looks_two,
+              color: Colors.blue,
+            ),
+          ),
+        ],
+        controller: controller,
+      ),
+    );
+  }
+}
